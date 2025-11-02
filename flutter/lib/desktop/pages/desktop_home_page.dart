f79041627e22b7abdfc0ef39ef0e20c7a3f4ab8c
huzhouqing <396358531@qq.com>
2025-11-02 13:21:18 +0800

    update

--- flutter/lib/desktop/pages/desktop_home_page.dart
+++ flutter/lib/desktop/pages/desktop_home_page.dart
@@ -456,21 +456,21 @@ class _DesktopHomePageState extends State<DesktopHomePage>
     }
 
     if (isWindows && !bind.isDisableInstallation()) {
-      if (!bind.mainIsInstalled()) {
-        return buildInstallCard(
-            "", bind.isOutgoingOnly() ? "" : "install_tip", "Install",
-            () async {
-          await rustDeskWinManager.closeAllSubWindows();
-          bind.mainGotoInstall();
-        });
-      } else if (bind.mainIsInstalledLowerVersion()) {
-        return buildInstallCard(
-            "Status", "Your installation is lower version.", "Click to upgrade",
-            () async {
-          await rustDeskWinManager.closeAllSubWindows();
-          bind.mainUpdateMe();
-        });
-      }
+//       if (!bind.mainIsInstalled()) {
+//         return buildInstallCard(
+//             "", bind.isOutgoingOnly() ? "" : "install_tip", "Install",
+//             () async {
+//           await rustDeskWinManager.closeAllSubWindows();
+//           bind.mainGotoInstall();
+//         });
+//       } else if (bind.mainIsInstalledLowerVersion()) {
+//         return buildInstallCard(
+//             "Status", "Your installation is lower version.", "Click to upgrade",
+//             () async {
+//           await rustDeskWinManager.closeAllSubWindows();
+//           bind.mainUpdateMe();
+//         });
+//       }
     } else if (isMacOS) {
       final isOutgoingOnly = bind.isOutgoingOnly();
       if (!(isOutgoingOnly || bind.mainIsCanScreenRecording(prompt: false))) {
