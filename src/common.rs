1a0e040c12fdd78516f35b87e277eefe8e08f3f2
huzhouqing <396358531@qq.com>
2025-11-10 10:45:06 +0800

    update

--- src/common.rs
+++ src/common.rs
@@ -1024,7 +1024,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://106.12.34.209".to_owned()
+    "http://remote.jnyx.top".to_owned()
 }
 
 #[inline]
